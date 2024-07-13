7e702abf8dbe20f6e40b66f1375a0c695d833a91
mizzo <samuelhope805@gmail.com>
2024-07-13 21:53:16 +0800

    first commit

---
+++ config/db.js
@@ -0,0 +1,29 @@
+/*const mongoose = require("mongoose");
+try {
+  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+  console.log("Database Connected Successfully");
+} catch (err) {
+  console.log("Database Not Connected");
+}*/
+
+const mongoose = require("mongoose");
+
+// Replace the following with your actual MongoDB connection string
+const remoteDbUrl = "mongodb+srv://Mizzo:Sammy2001%40@cluster0.bvaggu8.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(remoteDbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+.then(() => {
+  console.log("Database Connected Successfully");
+})
+.catch((err) => {
+  console.log("Database Not Connected", err);
+});
+
