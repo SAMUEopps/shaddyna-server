f789cce3890ef30fdf2193e470ebc87cb40ac7c1
mizzo <samuelhope805@gmail.com>
2024-07-30 15:30:43 +0300

    made changes

---
+++ app.js
@@ -0,0 +1,92 @@
+/* 
+
+================== Most Important ==================
+* Issue 1 :
+In uploads folder you need create 3 folder like bellow.
+Folder structure will be like: 
+public -> uploads -> 1. products 2. customize 3. categories
+*** Now This folder will automatically create when we run the server file
+
+* Issue 2:
+For admin signup just go to the auth 
+controller then newUser obj, you will 
+find a role field. role:1 for admin signup & 
+role: 0 or by default it for customer signup.
+go user model and see the role field.
+
+*/
+
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+// Import Router
+const authRouter = require("./routes/auth");
+const categoryRouter = require("./routes/categories");
+const productRouter = require("./routes/products");
+//const brainTreeRouter = require("./routes/braintree").default;
+const orderRouter = require("./routes/orders");
+const usersRouter = require("./routes/users");
+const customizeRouter = require("./routes/customize");
+// Import Auth middleware for check user login or not~
+const { loginCheck } = require("./middleware/auth");
+const CreateAllFolder = require("./config/uploadFolderCreateScript");
+
+/* Create All Uploads Folder if not exists | For Uploading Images */
+CreateAllFolder();
+
+// Database Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    console.log(
+      "==============Mongodb Database Connected Successfully=============="
+    )
+  )
+  .catch((err) => console.log("Database Not Connected !!!", err));
+
+// Middleware
+app.use(morgan("dev"));
+app.use(cookieParser());
+//app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// CORS Configuration
+const allowedOrigins = ['http://192.168.0.109:3000','http://localhost:3000', 'https://shaddyna-client.onrender.com'];
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
+
+
+// Routes
+app.use("/api", authRouter);
+app.use("/api/user", usersRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/product", productRouter);
+//app.use("/api", brainTreeRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/customize", customizeRouter);
+
+// Run Server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log("Server is running on ", PORT);
+});
